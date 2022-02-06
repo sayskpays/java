9c3d229767b57aea0c421104698c5121dda7e10f
sayskpays <76785132+sayskpays@users.noreply.github.com>
2022-02-06 20:30:18 +0900

    .

--- pythonStudy/string/StringFormating.py
+++ pythonStudy/string/StringFormating.py
@@ -2,4 +2,9 @@
 print("I eat %d apples" %3 )
 # 문자열 대입
 print("I eat %s apples" %"five")
+# 변수로 저장 후 대입 
+number = 3
+name = "hong"
+print("I eat %d apples" %number)
+print("My name is %s and I want to buy an %d apples" %(name,number))
 

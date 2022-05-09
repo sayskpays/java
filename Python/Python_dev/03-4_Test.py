9affdf84a53ca989a5a305dc752507e7ef54227c
sayskpays <sayskpayss@naver.com>
2022-05-09 17:55:23 +0900

    .

--- Python/Python_dev/03-4_Test.py
+++ Python/Python_dev/03-4_Test.py
@@ -21,10 +21,40 @@ while flag:
     if num  % 3 == 0 :
          hap.append(num)
          print(hap)
-              
     elif num > 1000:
         flag = False
         break
-        
+    
+# Q3. *찍는 프로그램을 만들어 보자.
+
+star = '*'
+flag = True
+num = 1 
+while flag:
+    print(star*num)
+    num += 1
+    if num > 5 :
+        flag = False
+        break
+    
+# Q4. for 문을 사용해 1부터 100까지 숫자를 출력해보자.
+
+result = [x for x in range(1,1000)]
+print(result)
+
+"""
+Q5. A 학급에 총 10명의 학생이 있다. 이 학생들의
+중간고사 점수는 다음과 같다
+[70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+for문을 사용하여 A 학급의 평균 점수를 구해보자.
+"""
+
+class_list = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+
+for x in class_list:
+    
+    class_avg = x / len(class_list)
+    print(class_avg)    
+
     
     

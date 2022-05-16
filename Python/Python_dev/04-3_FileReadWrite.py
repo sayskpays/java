c850cac797a3555296e4e1a16a014740b2f3dfbc
sayskpays <sayskpayss@naver.com>
2022-05-16 18:01:21 +0900

    .

--- Python/Python_dev/04-3_FileReadWrite.py
+++ Python/Python_dev/04-3_FileReadWrite.py
@@ -6,6 +6,11 @@ f = open("new_file.txt",'w')
 f.close()
 
 # 파일 열기 모드 종류
-# r - 읽기모드 w
+# r - 읽기모드 w - 쓰기모드 a - 파일의 마지막에 새로운 내용을 추가 시킬 때
+# / or \\ 로 경로 지정
+f = open("C:/doit/새파일.txt", 'w')
+f.close()
+
+
 
 

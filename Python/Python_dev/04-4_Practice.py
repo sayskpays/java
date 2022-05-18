4cc6d46d9d48d11aa3337cd91f1bff24fe705b6a
sayskpays <76785132+sayskpays@users.noreply.github.com>
2022-05-19 01:35:57 +0900

    .

--- Python/Python_dev/04-4_Practice.py
+++ Python/Python_dev/04-4_Practice.py
@@ -17,4 +17,11 @@ check_num = is_odd(5)
 # Q2 . 입력으로 들어오는 모든 수의 평균 값을 계산해 주는 함수를 작성해 보자.(단 입력으로 들어오는 수의 개수는 정해져 있지 않다.)
 # * 평균 값을 구할 때 len 함수를 사용해 보자.
 
+def insert_avg(*args):
+    result = 0
+    for i in args:
+        result = result + args / len(args)
+    return result
 
+print(insert_avg(1,2,3,4,5))
+    

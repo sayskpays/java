ecdf1846d7d574384b66c08c9f86ba0c46234fd2
sayskpays <sayskpayss@naver.com>
2022-05-18 13:13:21 +0900

    .

--- Python/Python_dev/00_Test_Log_File_Move.py
+++ Python/Python_dev/00_Test_Log_File_Move.py
@@ -5,21 +5,17 @@ import shutil
 
 # Filed
 
-source = r'C:\tibco\tsnm\logs'
-destination = r'C:\tibco\logs'
+source = r'C:\test'
+destination = r'C:\test\logs'
 files = os.listdir(source)
 
 def get_files_count(folder_path):
     dirList = os.listdir(folder_path)
     return len(dirList)
 
-if __name__=="__main__":
-    print(get_files_count("."))
-    
-    
-if get_files_count > 1:
+if get_files_count :
     for file in files:
         new_path = shutil.move(f"{source}/{file}", destination)
-        print(new_path)
+        print("Moved File Path : %s " % new_path)
     
     

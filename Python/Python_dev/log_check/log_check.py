ed6eac0d19dfb4ed81654ea892965161affc7042
sayskpays <sayskpayss@naver.com>
2022-05-31 10:54:16 +0900

    .

--- Python/Python_dev/log_check/log_check.py
+++ Python/Python_dev/log_check/log_check.py
@@ -12,31 +12,49 @@ read_file_path = "C:/dev/hello.txt"
 def searchFile(interface_id):
     
     str_data = ""
+    flag = True
+    
     
     print("Input IF ID : %s" %interface_id.upper())
+    
     with fileinput.input(glob.glob(read_file_path)) as f:
         for line in f:
-            if line.find(interface_id.casefold()) !=-1 :
-                str_data = line
+           
+            
+            # for interface_id in enumerate(line):    
+            #     print(interface_id)
+                # if line.find(interface_id) !=-1 :
+                #     list_data = list()
+                #     list_data.append(datetime.strptime(line[:24],'%Y %b %d %H:%M:%S:%f'))
+                #     print(list_data)
+                # else : 
+                #     pass
+                       
+        #     if line.find(interface_id.casefold()) !=-1 :
+        #         str_data = line
                 
-                error_time = datetime.strptime(str_data[:24].replace(" ",""),'%Y%b%d%H:%M:%S:%f')
-                error_check_time = error_time+timedelta(minutes=3)
-                
-                print("Error Occurrence Time : %s " %str_data[:24])
-                print("Error Data : %s " %line)
-            else:
-                pass
-        
+        #         error_time = datetime.strptime(str_data[:24],'%Y %b %d %H:%M:%S:%f')
+
+        #         print(error_time)               
+
+        #         while(flag):
+        #             error_time = error_time + timedelta(milliseconds=1)
+        #             time_result = error_time.strftime('%Y %b %d %H:%M:%S:%f')
+        #             # print(time_result)
                     
+        #             if line.find(time_result) == -1:
+        #                 # print(time_result)
+        #                 flag = False
+        #             else:
+        #                 pass
+            
+        #     else:
+        #         pass
+            
+        # print("Error Occurrence Time : %s " %line[:24])
+        # print("Error Data : %s " %line)
+
 searchFile(str(input("insert data: ")))
                 
-"""
-from datetime import datetime, timedelta
-
-previous_date = datetime.now() - timedelta(days=1)
-current_date = datetime.now()
-print(present > past)
-
-"""
 
     

aca9cfcfae8378f8b05bdd21ce2894d8f2be897d
sayskpays <sayskpayss@naver.com>
2022-05-31 17:43:17 +0900

    .

--- Python/Python_dev/log_check/log_check.py
+++ Python/Python_dev/log_check/log_check.py
@@ -11,50 +11,58 @@ read_file_path = "C:/dev/hello.txt"
 
 def searchFile(interface_id):
     
-    str_data = ""
-    flag = True
-    
-    
-    print("Input IF ID : %s" %interface_id.upper())
-    
     with fileinput.input(glob.glob(read_file_path)) as f:
         for line in f:
            
+            str_data = line.lower()
+            interface_id = interface_id.lower()
+            list_all_data = list(str_data.split("\n"))
+            list_check_data = list()
             
-            # for interface_id in enumerate(line):    
-            #     print(interface_id)
-                # if line.find(interface_id) !=-1 :
-                #     list_data = list()
-                #     list_data.append(datetime.strptime(line[:24],'%Y %b %d %H:%M:%S:%f'))
-                #     print(list_data)
-                # else : 
-                #     pass
-                       
-        #     if line.find(interface_id.casefold()) !=-1 :
-        #         str_data = line
+            for x in list_all_data:
+                if interface_id in x:
+                    list_check_data.append(x)
+            print(list_check_data)
+            
+            # print("%s" %line[:24])
+            # print("%s" %line)
+            
+            # print(str_data)
+            # print(type(str_data))
+            
                 
-        #         error_time = datetime.strptime(str_data[:24],'%Y %b %d %H:%M:%S:%f')
-
-        #         print(error_time)               
-
-        #         while(flag):
-        #             error_time = error_time + timedelta(milliseconds=1)
-        #             time_result = error_time.strftime('%Y %b %d %H:%M:%S:%f')
-        #             # print(time_result)
+            # if line.find(interface_id) !=-1 :
+            #     list_data = list()
+            #     for interface_id in line:
+            #         # datetime.strptime(line[:24],'%Y %b %d %H:%M:%S:%f')
+            #         list_data.append(line)
                     
-        #             if line.find(time_result) == -1:
-        #                 # print(time_result)
-        #                 flag = False
-        #             else:
-        #                 pass
-            
-        #     else:
-        #         pass
-            
-        # print("Error Occurrence Time : %s " %line[:24])
-        # print("Error Data : %s " %line)
+            #     print(list_data)
+            # else : 
+            #     pass
+                    
+            # if line.find(interface_id.casefold()) !=-1 :
+            #     str_data = line
+                
+            #     error_time = datetime.strptime(str_data[:24],'%Y %b %d %H:%M:%S:%f')
 
-searchFile(str(input("insert data: ")))
+            #     print(error_time)               
+
+            #     while(flag):
+            #         error_time = error_time + timedelta(milliseconds=1)
+            #         time_result = error_time.strftime('%Y %b %d %H:%M:%S:%f')
+            #         # print(time_result)
+                    
+            #         if line.find(time_result) == -1:
+            #             # print(time_result)
+            #             flag = False
+            #         else:
+            #             pass
+            # else:
+            #     pass
+            
+
+searchFile(input("insert data: "))
                 
 
     

52e7a013780ac7622e4ed553f057199e1fff12cd
sayskpays <sayskpayss@naver.com>
2022-06-08 17:35:14 +0900

    .

--- Python/Python_dev/log_check/expired_check.py
+++ Python/Python_dev/log_check/expired_check.py
@@ -7,38 +7,45 @@ def searchFile():
     EMS_ERROR_MESSAGE = 'sche'
     flag = False
     
-    with open(EMS_LOG_PATH,'r')as f:
-        for line in f:
-            list_all = list(line.lower().split("\n"))
-            
-            for data in list_all:
-                print(data)
-                if data.find(EMS_ERROR_MESSAGE) != -1 :
-                    flag = True
-                else:
-                    break
+    try :
+        with open(EMS_LOG_PATH,'r')as f:
+            for line in f:
+                list_all = list(line.lower().split("\n"))
+                
+                for data in list_all:
+                    print(data)
+                    if data.find(EMS_ERROR_MESSAGE) != -1 :
+                        flag = True
+                    else:
+                        break
+    except FileNotFoundError or FileExistsError:
+        print('========= Log File Not Exsist ===========')    
+        
     return flag
 
 searchFile()
 
 def excute(flag):
     if flag == True:
-        os.chdir('/home/tibco/tibco/tra/5.11/bin/')
-        print('=============EAI Process Stop================')
-        os.system('./AppManage -stop -app EAI_FBL -user a -pw a -domain eai_domain')
-        
-        print('==================EMS KILL==================')
-        os.system("kill $(ps -ef | grep 'tibemsd_8111' | grep -v 'color' | awk '{print $2}')")
-        os.system("kill $(ps -ef | grep 'tibemsd.conf' | grep -v 'color' | awk '{print $2}')")
-        
-        print('=================EMS RESTART===================')
-        os.chdir('/home/tibco/tibco/ems/8.4/bin/')
-        subprocess.call(['sh ./tibemsd_8111.sh &'],shell=True)
-        
-        print('===============EAI Process restart================')
-        os.chdir('/home/tibco/tibco/tra/5.11/bin/')
-        os.system('./AppManage -start -app EAI_FBL -user a -pw a -domain eai_domain')
-        print("================RESTART COMPLETED===================")
+        try : 
+            os.chdir('/home/tibco/tibco/tra/5.11/bin/')
+            print('=============EAI Process Stop================')
+            os.system('./AppManage -stop -app EAI_FBL -user a -pw a -domain eai_domain')
+            
+            print('==================EMS KILL==================')
+            os.system("kill $(ps -ef | grep 'tibemsd_8111' | grep -v 'color' | awk '{print $2}')")
+            os.system("kill $(ps -ef | grep 'tibemsd.conf' | grep -v 'color' | awk '{print $2}')")
+            
+            print('=================EMS RESTART===================')
+            os.chdir('/home/tibco/tibco/ems/8.4/bin/')
+            subprocess.call(['sh ./tibemsd_8111.sh &'],shell=True)
+            
+            print('===============EAI Process restart================')
+            os.chdir('/home/tibco/tibco/tra/5.11/bin/')
+            os.system('./AppManage -start -app EAI_FBL -user a -pw a -domain eai_domain')
+            print("================RESTART COMPLETED===================")
+        except FileExistsError or FileExistsError:
+            print('=========== EAI File Not Exsist===========') 
     else:
         print('============Does Not Exsist Expired Error Message==============')
 

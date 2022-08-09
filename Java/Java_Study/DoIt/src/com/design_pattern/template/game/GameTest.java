21f3b303e9104d092b2726ab407da368129d60d9
hong <sayskpayss@naver.com>
2022-08-09 17:42:04 +0900

    .

--- Java/Java_Study/DoIt/src/com/design_pattern/template/game/GameTest.java
+++ Java/Java_Study/DoIt/src/com/design_pattern/template/game/GameTest.java
@@ -2,6 +2,15 @@ package com.design_pattern.template.game;
 
 public class GameTest {
     public static void main(String[] args) {
-        Player player = new Player();
+        Player player = new Player(); // 처음 생성하면 Beginner Lv
+        player.play(1);
+
+        Advanced aLevel = new Advanced();
+        player.upgradeLevel(aLevel);
+        player.play(2);
+
+        Super sLevel = new Super();
+        player.upgradeLevel(sLevel);
+        player.play(3);
     }
 }
--- Java/Java_Study/DoIt/src/com/design_pattern/template/game/Player.java
+++ Java/Java_Study/DoIt/src/com/design_pattern/template/game/Player.java
@@ -14,9 +14,9 @@ public class Player {
     }
 
     // 모든 레벨(Beginner, Advanced, Super)이 매개 변수로 대입될 수 있기 때문에 모든 레벨의 상위 클래스인
-    // PlayerLevel을 매개변수의 자료형으로 받음
+    // PlayerLevel을 매개변수의 자료형(PlayerLevel을 상속받은 자식 클래스는 모두 가능)으로 받음
     public void upgradeLevel(PlayerLevel level){
-        this.level = level; // 현재 자신의 level을 매개 변수로 받은 level로 변경
+        this.level = level; // 현재 자신의 level을 매개 변수로 받은 level로 변경 (Ex  Beginner -> Advanced )
         level.showLevelMessage();
     }
 

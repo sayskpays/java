2cbd0e807880fcc0656e52294e9696d4cbfc3c49
hong <sayskpayss@naver.com>
2022-09-16 15:24:46 +0900

    .

--- Java/Java_Study/DoIt/src/App.java
+++ Java/Java_Study/DoIt/src/App.java
@@ -6,55 +6,34 @@ import java.util.Arrays;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void returnText(int a, int b){
 
-
-//        String rawString = "hi";
-//        byte[] bytes = rawString.getBytes("euc-kr");
-//
-//        String utf8EncodedString = new String(bytes, "ksc5601");
-//        System.out.println(utf8EncodedString);
-
-
-
-
-        String word = "신한은행.pdf";
-        System.out.println("utf-8 -> euc-kr        : " + new String(word.getBytes(StandardCharsets.UTF_8), "euc-kr"));
-        System.out.println("utf-8 -> ksc5601       : " + new String(word.getBytes(StandardCharsets.UTF_8), "ksc5601"));
-        System.out.println("utf-8 -> x-windows-949 : " + new String(word.getBytes(StandardCharsets.UTF_8), "x-windows-949"));
-        System.out.println("utf-8 -> iso-8859-1    : " + new String(word.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
-
-        System.out.println("iso-8859-1 -> euc-kr        : " + new String(word.getBytes(StandardCharsets.ISO_8859_1), "euc-kr"));
-        System.out.println("iso-8859-1 -> ksc5601       : " + new String(word.getBytes(StandardCharsets.ISO_8859_1), "ksc5601"));
-        System.out.println("iso-8859-1 -> x-windows-949 : " + new String(word.getBytes(StandardCharsets.ISO_8859_1), "x-windows-949"));
-        System.out.println("iso-8859-1 -> utf-8         : " + new String(word.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
-
-        System.out.println("euc-kr -> utf-8         : " + new String(word.getBytes("euc-kr"), StandardCharsets.UTF_8));
-        System.out.println("euc-kr -> ksc5601       : " + new String(word.getBytes("euc-kr"), "ksc5601"));
-        System.out.println("euc-kr -> x-windows-949 : " + new String(word.getBytes("euc-kr"), "x-windows-949"));
-        System.out.println("euc-kr -> iso-8859-1    : " + new String(word.getBytes("euc-kr"), StandardCharsets.ISO_8859_1));
-
-        System.out.println("ksc5601 -> euc-kr        : " + new String(word.getBytes("ksc5601"), "euc-kr"));
-        System.out.println("ksc5601 -> utf-8         : " + new String(word.getBytes("ksc5601"), StandardCharsets.UTF_8));
-        System.out.println("ksc5601 -> x-windows-949 : " + new String(word.getBytes("ksc5601"), "x-windows-949"));
-        System.out.println("ksc5601 -> iso-8859-1    : " + new String(word.getBytes("ksc5601"), StandardCharsets.ISO_8859_1));
-
-        System.out.println("x-windows-949 -> euc-kr     : " + new String(word.getBytes("x-windows-949"), "euc-kr"));
-        System.out.println("x-windows-949 -> utf-8      : " + new String(word.getBytes("x-windows-949"), StandardCharsets.UTF_8));
-        System.out.println("x-windows-949 -> ksc5601    : " + new String(word.getBytes("x-windows-949"), "ksc5601"));
-        System.out.println("x-windows-949 -> iso-8859-1 : " + new String(word.getBytes("x-windows-949"), StandardCharsets.ISO_8859_1));
-//
-
-
-
-
-
+        if(a > b){
+            System.out.println(">");
+        }else if(a < b){
+            System.out.println("<");
+        }else {
+            System.out.println("==");
+        }
     }
 
 
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        int a = scan.nextInt();
+        int b = Integer.parseInt(scan.next());
+
+
+        returnText(a,b);
+
+    }
 
 }

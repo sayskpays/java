10e23bc445dd0f170a9256d20fca3fcf2f540933
hong <sayskpayss@naver.com>
2022-11-10 11:40:49 +0900

    .

---
+++ Java/CS/src/cs/design_pattern/Singleton.java
@@ -0,0 +1,18 @@
+package cs.design_pattern;
+
+public class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}

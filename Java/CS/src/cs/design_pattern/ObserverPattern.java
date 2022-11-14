3d88fde3604659a81022c5e02f4356db2b4bfe89
hong <sayskpayss@naver.com>
2022-11-14 15:26:05 +0900

    .

--- Java/CS/src/cs/design_pattern/ObserverPattern.java
+++ Java/CS/src/cs/design_pattern/ObserverPattern.java
@@ -45,10 +45,45 @@ class Topic implements Subject{
 
     @Override
     public Object getUpdate(Observer obj) {
-        return null;
+        return this.message;
+    }
+
+    public void postMessage(String msg){
+        System.out.println("Message sended to Topic:" + msg);
+        this.message = msg;
+        notifyObservers();
+    }
+}
+
+class TopicSubscriber implements Observer{
+    private String name;
+    private Subject topic;
+
+    public TopicSubscriber(String name, Subject topic) {
+        this.name = name;
+        this.topic = topic;
+    }
+
+    @Override
+    public void update() {
+        String msg = (String) topic.getUpdate(this);
+        System.out.println(name + ":: got message >> " + msg);
+
     }
 }
 
 public class ObserverPattern {
+    public static void main(String[] args) {
+        Topic topic = new Topic();
 
+        Observer aObserver = new TopicSubscriber("a", topic);
+        Observer bObserver = new TopicSubscriber("b", topic);
+        Observer cObserver = new TopicSubscriber("c", topic);
+
+        topic.register(aObserver);
+        topic.register(bObserver);
+        topic.register(cObserver);
+
+        topic.postMessage("amumu");
+    }
 }
